0f46f4429673384b1e4492ff03e69ff3b785c0ff
Ryan Young <youngrya001@gmail.com>
2024-03-30 18:02:06 -0400

    Add files via upload
    paddle sound, player score sound and cpu score sound implemented.

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,7 +1,13 @@
 extends Sprite2D
 
 var score := [0, 0]# 0:Player, 1: CPU
+# Speed at which the paddles move.
 const PADDLE_SPEED : int = 500
+
+# goal sound
+@onready var scorePlayerSound = $scorePlayer
+@onready var scoreCpuSound = $scoreCPU
+
 
 func _on_ball_timer_timeout():
 	$Ball.new_ball()
@@ -9,11 +15,13 @@ func _on_ball_timer_timeout():
 func _on_score_left_body_entered(body):
 	score [1] += 1
 	$Hud/CPUScore.text = str(score[1])
+	scoreCpuSound.play()
 	$ballTimer.start()
-	$AudioStreamPlayer2D
+	
+
 
 func _on_score_right_body_entered(body):
 	score [0] += 1
 	$Hud/PlayerScore.text = str(score[0])
 	$ballTimer.start()
-	$AudioStreamPlayer2D
+	scorePlayerSound.play()
